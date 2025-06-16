4467bf7afd332b482f1b70b7b1c0f08d97693d4d
NidalGhonaim <nidalghonaim@gmail.com>
2025-06-16 13:33:13 +0100

    Refactor Terms component to use grid layout and add expand/collapse functionality

--- components/terms.tsx
+++ components/terms.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import Button from "./button";
 import styles from "./terms.module.scss";
+import classNames from "classnames";
 
 const Terms = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggle = () => setExpanded((current) => !current);
+
   return (
-    <div className={styles.container}>
+    <div
+      className={classNames(styles.container, { [styles.expanded]: expanded })}
+    >
       <p className={styles.title}>Casino Terms</p>
       <p className={styles.terms}>
         {`Lorem ipsum dolor sit amet consectetur adipiscing elit. Porta elementum
@@ -27,6 +38,85 @@ const Terms = () => {
         pulvinar vivamus. Primis vulputate ornare sagittis vehicula praesent dui
         felis. Vestibulum fusce dictum risus blandit quis suspendisse`}
       </p>
+      <div className={styles.rest}>
+        <p className={styles.terms}>
+          {`Lorem ipsum dolor sit amet consectetur adipiscing elit. Porta elementum
+        a enim euismod quam justo lectus. Dis parturient montes nascetur
+        ridiculus mus donec rhoncus. Vitae pellentesque sem placerat in id
+        cursus mi. Velit aliquam imperdiet mollis nullam volutpat porttitor
+        ullamcorper. Mattis scelerisque maximus eget fermentum odio phasellus
+        non. Tempus leo eu aenean sed diam urna tempor. Turpis fames primis
+        vulputate ornare sagittis vehicula praesent. Mauris pharetra vestibulum
+        fusce dictum risus blandit quis. Nec metus bibendum egestas iaculis
+        massa nisl malesuada. Proin libero feugiat tristique accumsan maecenas
+        potenti ultricies. Consequat magna ante condimentum neque at luctus
+        nibh. Ut hendrerit semper vel class aptent taciti sociosqu. Suscipit
+        auctor curabitur facilisi cubilia curae hac habitasse. Interdum tortor
+        ligula congue sollicitudin erat viverra ac. Conubia nostra inceptos
+        himenaeos orci varius natoque penatibus. Dolor sit amet consectetur
+        adipiscing elit quisque faucibus. Aenim euismod quam justo lectus
+        commodo augue. Montes nascetur ridiculus mus donec rhoncus eros
+        lobortis. Sem placerat in id cursus mi pretium tellus. Imperdiet mollis
+        nullam volutpat porttitor ullamcorper rutrum gravida. Maximus eget
+        fermentum odio phasellus non purus est. Eu aenean sed diam urna tempor
+        pulvinar vivamus. Primis vulputate ornare sagittis vehicula praesent dui
+        felis. Vestibulum fusce dictum risus blandit quis suspendisse`}
+        </p>
+        <p className={styles.terms}>
+          {`Lorem ipsum dolor sit amet consectetur adipiscing elit. Porta elementum
+        a enim euismod quam justo lectus. Dis parturient montes nascetur
+        ridiculus mus donec rhoncus. Vitae pellentesque sem placerat in id
+        cursus mi. Velit aliquam imperdiet mollis nullam volutpat porttitor
+        ullamcorper. Mattis scelerisque maximus eget fermentum odio phasellus
+        non. Tempus leo eu aenean sed diam urna tempor. Turpis fames primis
+        vulputate ornare sagittis vehicula praesent. Mauris pharetra vestibulum
+        fusce dictum risus blandit quis. Nec metus bibendum egestas iaculis
+        massa nisl malesuada. Proin libero feugiat tristique accumsan maecenas
+        potenti ultricies. Consequat magna ante condimentum neque at luctus
+        nibh. Ut hendrerit semper vel class aptent taciti sociosqu. Suscipit
+        auctor curabitur facilisi cubilia curae hac habitasse. Interdum tortor
+        ligula congue sollicitudin erat viverra ac. Conubia nostra inceptos
+        himenaeos orci varius natoque penatibus. Dolor sit amet consectetur
+        adipiscing elit quisque faucibus. Aenim euismod quam justo lectus
+        commodo augue. Montes nascetur ridiculus mus donec rhoncus eros
+        lobortis. Sem placerat in id cursus mi pretium tellus. Imperdiet mollis
+        nullam volutpat porttitor ullamcorper rutrum gravida. Maximus eget
+        fermentum odio phasellus non purus est. Eu aenean sed diam urna tempor
+        pulvinar vivamus. Primis vulputate ornare sagittis vehicula praesent dui
+        felis. Vestibulum fusce dictum risus blandit quis suspendisse`}
+        </p>
+        <p className={styles.terms}>
+          {`Lorem ipsum dolor sit amet consectetur adipiscing elit. Porta elementum
+        a enim euismod quam justo lectus. Dis parturient montes nascetur
+        ridiculus mus donec rhoncus. Vitae pellentesque sem placerat in id
+        cursus mi. Velit aliquam imperdiet mollis nullam volutpat porttitor
+        ullamcorper. Mattis scelerisque maximus eget fermentum odio phasellus
+        non. Tempus leo eu aenean sed diam urna tempor. Turpis fames primis
+        vulputate ornare sagittis vehicula praesent. Mauris pharetra vestibulum
+        fusce dictum risus blandit quis. Nec metus bibendum egestas iaculis
+        massa nisl malesuada. Proin libero feugiat tristique accumsan maecenas
+        potenti ultricies. Consequat magna ante condimentum neque at luctus
+        nibh. Ut hendrerit semper vel class aptent taciti sociosqu. Suscipit
+        auctor curabitur facilisi cubilia curae hac habitasse. Interdum tortor
+        ligula congue sollicitudin erat viverra ac. Conubia nostra inceptos
+        himenaeos orci varius natoque penatibus. Dolor sit amet consectetur
+        adipiscing elit quisque faucibus. Aenim euismod quam justo lectus
+        commodo augue. Montes nascetur ridiculus mus donec rhoncus eros
+        lobortis. Sem placerat in id cursus mi pretium tellus. Imperdiet mollis
+        nullam volutpat porttitor ullamcorper rutrum gravida. Maximus eget
+        fermentum odio phasellus non purus est. Eu aenean sed diam urna tempor
+        pulvinar vivamus. Primis vulputate ornare sagittis vehicula praesent dui
+        felis. Vestibulum fusce dictum risus blandit quis suspendisse`}
+        </p>
+      </div>
+      <Button
+        className={styles.button}
+        stretch
+        secondary={expanded}
+        onClick={toggle}
+      >
+        {expanded ? "Minimize" : "Read More"}
+      </Button>
     </div>
   );
 };
